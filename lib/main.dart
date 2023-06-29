7d47d04976427319db26a5f83e5e1ea248756bd5
Rakesh <rakeshbhandari@gmail.com>
2023-06-29 12:08:03 +0545

    final

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      //remove bug banner
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.lime,
